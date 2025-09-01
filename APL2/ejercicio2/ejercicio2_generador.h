3e8452d952173ea0cf5b845f9f7bedc14a81cdb9
Nahuel Alberto Rivas <nahuelrivas41@gmail.com>
2025-09-01 16:51:42 -0300

    Limpieza

--- APL2/ejercicio2/ejercicio2_generador.h
+++
@@ -1,8 +0,0 @@
-#ifndef GENERADOR_H
-#define GENERADOR_H
-
-#include <string>
-
-void hilo_generador(int id, int paquetes_por_hilo, const std::string& directorio);
-
-#endif
--- APL2/ejercicio2/ejercicio2_procesador.h
+++
@@ -1,8 +0,0 @@
-#ifndef PROCESADOR_H
-#define PROCESADOR_H
-
-#include <string>
-
-void hilo_procesador(const std::string& directorio, int id);
-
-#endif
